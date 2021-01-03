6c65d0595b8e786f6e6b1b24b18bf395c2bfab15
Jayme Waye <jaymewaye@gmail.com>
2021-01-02 19:19:01 -0500

    BE cards delete route fix

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -10,15 +10,16 @@ const {
 } = require('../controllers/cards');
 
 routes.get('/cards', getCards);
+routes.post('/cards', createCard);
 routes.delete('/cards/:cardId', deleteCard);
 routes.put('/cards/:cardId/likes', likeCard);
 routes.delete('/cards/:cardId/likes', dislikeCard);
 
-routes.post('/cards', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(20),
-    link: Joi.string().required().pattern(new RegExp('^https?:\\/\\/(www\\.)?[\\S^~<>]+\\.[\\S^~<>]+#?')),
-  }),
-}), createCard);
+// routes.post('/cards', celebrate({
+//   body: Joi.object().keys({
+//     name: Joi.string().required().min(2).max(20),
+//     link: Joi.string().required().pattern(new RegExp('^https?:\\/\\/(www\\.)?[\\S^~<>]+\\.[\\S^~<>]+#?')),
+//   }),
+// }), createCard);
 
 module.exports = routes;
